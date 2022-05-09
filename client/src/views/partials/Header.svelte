43b99e997d76cd97f23170503601de51d6cd0bb7
Jeff Rynhart <jeff.rynhart@du.edu>
2022-05-09 15:35:37 -0600

    Added DU logo template header

--- client/src/views/partials/Header.svelte
+++ client/src/views/partials/Header.svelte
@@ -1,1 +1,11 @@
-<h4>Header block</h4>
+<header>
+  <div class="w-100 header-logo-wrapper">
+    <!-- <div class="row no-side-margin">
+      <div class="col-md-12"> -->
+        <div class="header-logo">
+            <a href="https://library.du.edu" title="University Libraries" target="_blank"><img src="assets/img/du-logo2.gif" alt="University of Denver Logo" /></a>
+        </div>
+      <!-- </div>
+    </div> -->
+  </div>
+</header>
