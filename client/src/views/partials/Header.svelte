2e4de12fb7cac5c9d7aed68e3dcf81f9c16e60b4
Jeff Rynhart <jeff.rynhart@du.edu>
2022-05-26 15:57:02 -0600

    Added baseurl variable to Header file, to be set with current baseurl on all page loads

--- client/src/views/partials/Header.svelte
+++ client/src/views/partials/Header.svelte
@@ -1,9 +1,13 @@
+<script>
+  const baseUrl = `${window.location.protocol}//${window.location.host}`;
+</script>
+
 <header>
   <div class="w-100 header-logo-wrapper">
     <!-- <div class="row no-side-margin">
       <div class="col-md-12"> -->
         <div class="header-logo">
-            <a href="https://library.du.edu" title="University Libraries" target="_blank"><img src="assets/img/du-logo2.gif" alt="University of Denver Logo" /></a>
+            <a href="https://library.du.edu" title="University Libraries" target="_blank"><img src="{baseUrl}/assets/img/du-logo2.gif" alt="University of Denver Logo" /></a>
         </div>
       <!-- </div>
     </div> -->
