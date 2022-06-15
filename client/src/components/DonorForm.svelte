fb9a2c7f833a1b73e44733ad7650cc158e6acf69
Jeff Rynhart <jeff.rynhart@du.edu>
2022-06-15 17:30:30 -0600

    Updated form to bind to data object properties individually, to fix form field binding after donor data request delay

--- client/src/components/DonorForm.svelte
+++ client/src/components/DonorForm.svelte
@@ -1,79 +1,100 @@
 <script>
-  export let data;
+  import {Configuration} from '../config';
+  import {ajaxRequest} from '../libs/ajax.js';
+
+  export let data = {
+    title: "",
+    firstName: "",
+    lastName: "",
+    organization: "",
+    address1: "",
+    address2: "",
+    city: "",
+    state: "",
+    postalCode: "",
+    country: "",
+    phone: "",
+    email: ""
+  };
   export let donorId;
 
   let method = "post";
-  let action = "/donor";
+  let action = `${$Configuration.donorApiDomain}/donor`;
   let buttonText = "Add Donor";
 
   if(donorId) {
     method = "put";
+    action = `${$Configuration.donorApiDomain}/donor/${donorId}`;
     buttonText = "Update";
   }
 
-  console.log("Form data into DonorForm:", data)
+  const sanitizeFormData = (data) => {
+    // TODO map array, perform sanitization on each value
+    return data;
+  }
 
-  /* Init form fields from donor data */
-  var {
-    title= "",
-    lastName= "",
-    firstName= "",
-    organization= "",
-    address1= "",
-    address2= "",
-    city= "",
-    state= "",
-    postalCode= "",
-    country= "",
-    phone= "",
-    email= ""
-  } = data;
+  const onSubmitDonorForm = () => {
+    console.log("On submit: method/action:", method, action)
+    // TODO: Initiate feedback "Submitting..."
+    let formData = sanitizeFormData(data); // test
+    //let formData = sanitizeFormData({title, firstName, lastName, organization, address1, address2, city, state, postalCode, country, phone, email});
+    ajaxRequest(method, action, function(error, response, status) {
+      let message = "Error sending data";
+      if(error) console.error("Error:", error);
+      else if(status != 200) console.log("Response status: ", status);
+      else {
+        message = "New donor created";
+      }
+
+      // TODO: Initiate feedback (message)
+    }, formData);
+  }
 </script>
 
 <form id="donor-form" class="form" method="{method}" action="{action}">
   <div class="form-fields">
     <div class="form-group">
       <label for="title">Title</label>
-      <input type="text" class="form-control" id="title" bind:value={title}>
+      <input type="text" class="form-control" id="title" name="title" bind:value={data.title}>
 
       <label for="lastName">Last Name</label>
-      <input type="text" class="form-control" id="lastName" bind:value={lastName}>
+      <input type="text" class="form-control" id="lastName" name="lastName" bind:value={data.lastName}>
 
       <label for="firstName">First Name</label>
-      <input type="text" class="form-control" id="firstName" bind:value={firstName}>
+      <input type="text" class="form-control" id="firstName" name="firstName" bind:value={data.firstName}>
 
       <label for="organization">Organization</label>
-      <input type="text" class="form-control" id="organization" bind:value={organization}>
+      <input type="text" class="form-control" id="organization" name="organization" bind:value={data.organization}>
     </div>
     <div class="form-group">
       <label for="address1">Address 1</label>
-      <input type="text" class="form-control" id="address1" bind:value={address1}>
+      <input type="text" class="form-control" id="address1" name="address1" bind:value={data.address1}>
 
       <label for="address2">Address 2</label>
-      <input type="text" class="form-control" id="address2" bind:value={address2}>
+      <input type="text" class="form-control" id="address2" name="address2" bind:value={data.address2}>
 
       <label for="city">City</label>
-      <input type="text" class="form-control" id="city" bind:value={city}>
+      <input type="text" class="form-control" id="city" name="city" bind:value={data.city}>
 
       <label for="state">State</label>
-      <input type="text" class="form-control" id="state" bind:value={state}>
+      <input type="text" class="form-control" id="state" name="state" bind:value={data.state}>
     </div>
     <div class="form-group">
       <label for="postalCode">Postal Code</label>
-      <input type="text" class="form-control" id="postalCode" bind:value={postalCode}>
+      <input type="text" class="form-control" id="postalCode" name="postalCode" bind:value={data.postalCode}>
 
       <label for="country">Country</label>
-      <input type="text" class="form-control" id="country" bind:value={country}>
+      <input type="text" class="form-control" id="country" name="country" bind:value={data.country}>
 
       <label for="phone">Phone</label>
-      <input type="text" class="form-control" id="phone" bind:value={phone}>
+      <input type="text" class="form-control" id="phone" name="phone" bind:value={data.phone}>
 
       <label for="email">Email</label>
-      <input type="text" class="form-control" id="email" bind:value={email}>
+      <input type="text" class="form-control" id="email" name="email" bind:value={data.email}>
     </div>
   </div>
 
-  <button class="btn btn-default" id="update-data-button" type="submit">{buttonText}</button> <!-- on:click|preventDefault={onSubmitDonorForm} to add validation -->
+  <button class="btn btn-default" id="update-data-button" type="submit" on:click|preventDefault={onSubmitDonorForm}>{buttonText}</button> <!-- TODO: on:click|preventDefault={onSubmitDonorForm} to add validation -->
 </form>
 
 <style>
