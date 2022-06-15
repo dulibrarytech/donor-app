5dd7468bb014fc61cfacf5f1c4739e847e8b2556
Jeff Rynhart <jeff.rynhart@du.edu>
2022-06-15 12:17:03 -0600

    Added bodyparser urlencoded plugin

--- config/express.js
+++ config/express.js
@@ -5,10 +5,6 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { baseUrl } = require(`./${process.env.CONFIGURATION_FILE}`);
 
-//const transactionsRoutes = require("./server/routes/transactions");
-// const donorsRoutes = require("../server/routes/donors");
-// const donationsRoutes = require("../server/routes/donations");
-
 const donorRoutes = require("../server/donor/routes");
 const donationRoutes = require("../server/donation/routes");
 
@@ -16,9 +12,9 @@ module.exports = function () {
 
   app.use(cors());
   app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded());
   app.use(morgan('tiny'));
 
-  //app.use('/api/transactions', transactionsRoutes);
   app.use('/donor', donorRoutes);
   app.use('/donation', donationRoutes);
 
