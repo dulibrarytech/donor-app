4fd581126ba05bd93cd59f9425453074502b1e86
Jeff Rynhart <jeff.rynhart@du.edu>
2022-06-28 15:16:33 -0600

    Added ajax library

---
+++ client/src/libs/ajax.js
@@ -0,0 +1,23 @@
+/*
+ * Wrapper for Javascript fetch api funcitons
+ */
+
+'use-strict'
+
+export const ajaxRequest = (type, url, callback, data=null) => {
+  let options = {
+    method: type,
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+  }
+  if(data) options['body'] = JSON.stringify(data);
+
+  fetch(url, options)
+    .then(response => {
+      callback(null, response, response.status);
+    })
+    .catch(error => {
+      callback(error, null);
+    });
+}
