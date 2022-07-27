bc6e27d4d1145208fdfaa4468acb2fcaabd9012b
Jeff Rynhart <jeff.rynhart@du.edu>
2022-07-27 14:17:26 -0600

    Updated login form styles

--- client/src/components/LoginForm.svelte
+++ client/src/components/LoginForm.svelte
@@ -18,8 +18,6 @@
     <div class="form-group">
       <label for="login-username">Username</label>
       <input type="text" class="form-control" id="login-username" bind:value={formData.username} placeholder="Username">
-    </div>
-    <div class="form-group">
       <label for="login-password">Password</label>
       <input type="password" class="form-control" id="login-password" bind:value={formData.password} placeholder="Password">
     </div>
@@ -29,5 +27,9 @@
 </div>
 
 <style>
-
+@media (min-width: 768px) {
+  .login-form {
+    width: 50%
+  }
+}
 </style>
