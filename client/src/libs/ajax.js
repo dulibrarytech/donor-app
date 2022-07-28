cbb409e89e7b5b71df4b472472b6330269ce99a3
Jeff Rynhart <jeff.rynhart@du.edu>
2022-07-28 17:23:54 -0600

    Updated ajaxRequest function to accept an array for querystring parameters and values

--- client/src/libs/ajax.js
+++ client/src/libs/ajax.js
@@ -6,14 +6,30 @@
 
 import {Session} from './session.js';
 
-export const ajaxRequest = (type, url, callback, data=null) => {
-  let options = {
+export const ajaxRequest = (type, url, callback, data=null, options=null, query=null) => {
+  let defaultOptions = {
     method: type,
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
     },
   }
   if(data) options['body'] = JSON.stringify(data);
+
+  if(options) {
+    // TODO: Combine defaultOptions and options objects
+    // Output options
+  }
+  else options = defaultOptions;
+
+  if(query) {
+    let value, queryString = "";
+    Object.keys(query).forEach((param, index) => {
+      value = query[param];
+      if(index == 0) queryString += `?${param}=${value}`
+      else queryString += `&${param}=${value}`
+    });
+    url += queryString;
+  }
 
   if(Session.isSession()) {
     options.headers['authorization'] = Session.getToken();
@@ -26,4 +42,5 @@ export const ajaxRequest = (type, url, callback, data=null) => {
     .catch(error => {
       callback(error, null);
     });
+  callback(null, JSON.stringify([]))
 }
