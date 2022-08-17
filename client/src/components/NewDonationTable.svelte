5baa6410167933fc620461eddee6e96b21099f5e
Jeff Rynhart <jeff.rynhart@du.edu>
2022-08-17 17:18:23 -0600

    Updated 'complete' link text to 'mark as complete', applied fixed widths to td elements in new donation table (inbox page)

--- client/src/components/NewDonationTable.svelte
+++ client/src/components/NewDonationTable.svelte
@@ -28,8 +28,8 @@
 <thead>
   <tr>
     <th scope="col">Date of Donation</th>
-    <th scope="col">Last Name/Organization</th>
-    <th scope="col">First Name</th>
+    <th scope="col">Last Name</th>
+    <th scope="col">Organization</th>
     <th scope="col">Donation Type</th>
     <th scope="col">Status</th>
     <th scope="col"></th>
@@ -41,20 +41,20 @@
   {#if items.length > 0}
     {#each items as donation}
       <tr>
-        <td>{donation.dateOfGift?.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/) ?? "No date"}</td>
+        <td width="10%">{donation.dateOfGift?.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/) ?? "No date"}</td>
 
-        <td>{donation.lastName || donation.organization || ""}</td>
-        <td>{donation.firstName || ""}</td>
+        <td width="17.5%">{donation.lastName || ""}</td>
+        <td width="17.5%">{donation.organization || ""}</td>
 
-        <td>{formatGiftTypeField(donation.important) || "Unknown"}</td>
+        <td width="10%">{formatGiftTypeField(donation.important) || "Unknown"}</td>
 
-        <td>{#if statusUpdate == donation.id}Updating...{:else}{formatStatusField(donation.letter) || "Unknown"}{/if}</td>
+        <td width="10%">{#if statusUpdate == donation.id}Updating...{:else}{formatStatusField(donation.letter) || "Unknown"}{/if}</td>
 
-        <td><a href="/donation/{donation.id}">View Donation</a></td>
+        <td width="10%"><a href="/donation/{donation.id}">View Donation</a></td>
 
-        <td><a href="/donor/{donation.donorId}">View Donor</a></td>
+        <td width="10%"><a href="/donor/{donation.donorId}">View Donor</a></td>
 
-        <td><a href="#" on:click|preventDefault={(event) => onClickComplete(donation.id)} value={donation.id}>Complete</a></td>
+        <td width="15%"><a href="#" on:click|preventDefault={(event) => onClickComplete(donation.id)} value={donation.id}>Mark as Complete</a></td>
       </tr>
     {/each}
   {:else}
