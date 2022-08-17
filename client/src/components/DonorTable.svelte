5864000ec5f016aa3697d1e980b1d5fe1d15521e
Jeff Rynhart <jeff.rynhart@du.edu>
2022-08-17 10:09:12 -0600

    Added 'organization' column, changed 'last name' column to display only last name, set fixed td widths on donor table

--- client/src/components/DonorTable.svelte
+++ client/src/components/DonorTable.svelte
@@ -4,9 +4,9 @@
 
 <thead class="header">
   <tr>
-    <th scope="col">Last Name/Organization</th>
+    <th scope="col">Last Name</th>
+    <th scope="col">Organization</th>
     <th scope="col">First Name</th>
-    <!-- <th scope="col">Organization</th> -->
     <th scope="col"></th>
     <th scope="col"></th>
   </tr>
@@ -16,11 +16,11 @@
     {#each items as donor (donor.id)}
       {#if donor.id > 1} <!-- donor id 1 is reserved for anonymous donations -->
         <tr>
-          <td>{donor.lastName.length > 1 ? donor.lastName : donor.organization}</td>
-          <td>{donor.firstName}</td>
-          <!-- <td>{donor.organization ?? ""}</td> -->
-          <td><a href="/donor/{donor.id}">View</a></td>
-          <td><a href="/donation/donor/{donor.id}">Add Gift</a></td>
+          <td width="20%">{donor.lastName}</td>
+          <td width="30%">{donor.organization}</td>
+          <td width="25%">{donor.firstName}</td>
+          <td width="12.5%"><a href="/donor/{donor.id}">View</a></td>
+          <td width="12.5%"><a href="/donation/donor/{donor.id}">Add Gift</a></td>
         </tr>
       {/if}
     {/each}
