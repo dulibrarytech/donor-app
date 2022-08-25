c763f02b73a2fe10df4745811692b2ecc37b483d
Jeff Rynhart <jeff.rynhart@du.edu>
2022-08-25 16:57:34 -0600

    Added base path setting to client env and config

--- client/src/config.js
+++ client/src/config.js
@@ -2,9 +2,10 @@ import { writable } from 'svelte/store'
 
 export const Configuration = writable({
   runtimeEnv: process.env.RUNTIME_ENV,
+  domain: process.env.DONOR_APP_DOMAIN,
   basePath: process.env.DONOR_APP_BASE_PATH,
-  donorApiDomain: process.env.DONOR_APP_API_DOMAIN,
-  livingLibraryApiDomain: process.env.LIVINGLIBRARY_API_DOMAIN,
+  donorApiDomain: process.env.DONOR_APP_API_URL,
+  livingLibraryApiDomain: process.env.LIVINGLIBRARY_API_URL,
   livingLibraryApiKey: process.env.LIVINGLIBRARY_API_KEY,
   signatureImageFilename: process.env.DONOR_LETTER_SIGNATURE_IMAGE_FILENAME,
   letterSignatureName: process.env.LETTER_SIGNATURE_NAME,
