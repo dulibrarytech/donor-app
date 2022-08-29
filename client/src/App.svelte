353f976d3d8c6554f4392c06b6c60b355bfa6443
Jeff Rynhart <jeff.rynhart@du.edu>
2022-08-29 13:21:24 -0600

    Added base path to logout redirect

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -8,6 +8,7 @@
 	// Template Components
   import Header from "./views/partials/Header.svelte";
   import Footer from "./views/partials/Footer.svelte";
+	import Navbar from "./components/Navbar.svelte";
 
 	// Views
 	import Login from "./views/Login.svelte";
@@ -22,19 +23,15 @@
 	import LivingLibraryBookPlate from "./views/LivingLibraryBookPlate.svelte";
 	import NotFound from "./views/NotFound.svelte";
 
-  // View Components
-  import Navbar from "./components/Navbar.svelte";
-
 	let page;
 	let params;
+
+	const BASE_PATH = $Configuration.basePath || "";
 
 	const onNavigate = (event) => {
 		lscache.flush();
 	}
 
-	/*
-	 * Session functions
-	 */
 	const login = (data) => {
 		Session.create("donor_db", data.sessionData.token, data.sessionData.userData);
 		let {roleId} = data.sessionData.userData;
@@ -45,7 +42,7 @@
 	const logout = (requestPath = null) => {
 		if(Session.isSession("donor_db")) Session.destroy("donor_db");
 		lscache.flush();
-		window.location.replace(requestPath ? `/login?redirect=${requestPath}` : "/login");
+		window.location.replace(requestPath ? `${BASE_PATH}/login?redirect=${requestPath}` : `${BASE_PATH}/login`);
 	}
 
 	const onLogin = (event) => {
@@ -55,13 +52,7 @@
 	const onLogout = (event) => {
 		logout();
 	}
-	/*
-	 * End Session functions
-	 */
 
-	/*
-	 * Session validation middleware
-	 */
 	const validateSession = async (ctx, next) => {
 		if($Configuration.runtimeEnv == "production"|| $Configuration.runtimeEnv == "testing") {
 			let path = null;
@@ -81,84 +72,80 @@
 		}
 		else next()
 	}
-	/*
-	 * End Session validation middleware
-	 */
 
   /*
  	 * Router
  	 */
-	let basePath = $Configuration.basePath || "";
-	router(`${basePath}/`, () => {
-		window.location.replace(`${basePath}${$Configuration.landingPagePath}`);
+	router(`${BASE_PATH}/`, () => {
+		window.location.replace(`${BASE_PATH}${$Configuration.landingPagePath}`);
 	});
 
-	router(`${basePath}/login`, (ctx, next) => {
+	router(`${BASE_PATH}/login`, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = Login);
 
-	router(`${basePath}/logout`, (ctx, next) => {
+	router(`${BASE_PATH}/logout`, (ctx, next) => {
 		logout();
 		next();
 	}, () => page = Login);
 
-	router(`${basePath}/donors`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/donors`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = Donors); // <-- next()
 
-	router(`${basePath}/donor`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/donor`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = Donor); // <-- next()
 
-	router(`${basePath}/donor/:id`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/donor/:id`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = Donor);
 
-	router(`${basePath}/donations`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/donations`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = Donations);
 
-	router(`${basePath}/donation/donor/:donorId`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/donation/donor/:donorId`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = Donation);
 
-	router(`${basePath}/donation/:id`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/donation/:id`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = Donation);
 
-	router(`${basePath}/letter/:donorId/:donationId`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/letter/:donorId/:donationId`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = LetterView);
 
-	router(`${basePath}/inbox`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/inbox`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = Inbox);
 
-	router(`${basePath}/livingLibrary/donations`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/livingLibrary/donations`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = LivingLibraryDonations);
 
-	router(`${basePath}/livingLibrary/donation`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/livingLibrary/donation`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = LivingLibraryDonation);
 
-	router(`${basePath}/livingLibrary/donation/:id`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/livingLibrary/donation/:id`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = LivingLibraryDonation);
 
-	router(`${basePath}/livingLibrary/bookplate/:donationId`, validateSession, (ctx, next) => {
+	router(`${BASE_PATH}/livingLibrary/bookplate/:donationId`, validateSession, (ctx, next) => {
 		params = ctx.params;
 		next();
 	}, () => page = LivingLibraryBookPlate);
