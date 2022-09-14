d9fe82fb2407a9eab22888245aa67cdb61966084
Jeff Rynhart <jeff.rynhart@du.edu>
2022-09-14 17:34:34 -0600

    Added extended date-picker-svelte module to local project lib

---
+++ client/src/libs/date-picker-svelte/index.js
@@ -0,0 +1,5 @@
+export { default as DatePicker } from './DatePicker.svelte';
+export { default as DateInput } from './DateInput.svelte';
+
+/* UPDATE */
+export { localeFromDateFnsLocale } from './locale.js';
