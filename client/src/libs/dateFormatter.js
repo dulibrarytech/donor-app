bd4f1d017ebec976fbbb960f08387539d4439d0f
Jeff Rynhart <jeff.rynhart@du.edu>
2022-09-14 21:21:38 -0600

    Added iso string function to date formatter

--- client/src/libs/dateFormatter.js
+++ client/src/libs/dateFormatter.js
@@ -9,3 +9,8 @@ export const getNormalDateString = (dateObject) => {
 
   return monthNames[dateObject.getMonth()] + " " + dateObject.getDate() + ", " + dateObject.getFullYear();
 }
+
+export const getIsoDateString = (date) => {
+  const pad = n => n < 10 ? "0" + n : n;
+  return date.getFullYear() + "-" + pad(date.getMonth()+1) + "-" + pad(date.getDate());
+}
