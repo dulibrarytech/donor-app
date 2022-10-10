e0a45296f437fd218d7b9c4d558b5ab38a4de2b3
Jeff Rynhart <jeff.rynhart@du.edu>
2022-10-10 12:27:29 -0600

    Added form input length validation to login form and user feedback if username or password field is blank

--- client/src/views/Login.svelte
+++ client/src/views/Login.svelte
@@ -12,7 +12,6 @@
   const dispatch = createEventDispatcher();
   const urlParams = new URLSearchParams(window.location.search);
   var loginRedirectPath = null;
-  var sessionData = null;
   var messageDisplay;
 
   const init = () => {
@@ -24,27 +23,28 @@
     let data = event.detail;
     let url = `${$Configuration.donorApiDomain}/user/authenticate`;
 
-    ajaxRequest('POST', url, async function(error, response) {
-      if(error) {
-        console.log(`Authentication error: ${error}`);
-        messageDisplay.displayTimeoutMessage("Error");
-      }
-      else if(response.status == 200) {
-        console.log("Login successful");
-        let responseData = await response.json();
-        sessionData = responseData;
-      }
-      else if(response.status == 401){
-        console.log("Invalid credentials");
-        messageDisplay.displayTimeoutMessage("Invalid username or password");
-      }
-      else {
-        console.log("Authentication response status ", response.status);
-        messageDisplay.displayTimeoutMessage("Error");
-      }
-
-      dispatch('login', {sessionData, loginRedirectPath});
-    }, data);
+    if(data.username?.length > 0 && data.password?.length > 0) {
+      ajaxRequest('POST', url, async function(error, response) {
+        if(error) {
+          console.log(`Authentication error: ${error}`);
+          messageDisplay.displayTimeoutMessage("Error");
+        }
+        else if(response.status == 200) {
+          console.log("Login successful");
+          let sessionData = await response.json();
+          dispatch('login', {sessionData, loginRedirectPath});
+        }
+        else if(response.status == 401){
+          console.log("Invalid credentials");
+          messageDisplay.displayTimeoutMessage("Invalid username or password");
+        }
+        else {
+          console.log("Authentication response status ", response.status);
+          messageDisplay.displayTimeoutMessage("Error");
+        }
+      }, data);
+    }
+    else messageDisplay.displayTimeoutMessage("Please enter a username and password");
   }
 
   onMount(() => {
