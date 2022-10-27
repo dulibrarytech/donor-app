4d2316bbbbbbf63d82627108dc6cfce257dbe206
Jeff Rynhart <jeff.rynhart@du.edu>
2022-10-27 13:20:59 -0600

    Added handler to catch malformed request url parameters and return a 404, fixes error being returned in response that reveals app info

--- client/donor-app-client.js
+++ client/donor-app-client.js
@@ -10,6 +10,19 @@ if (process.env.RUNTIME_ENV === 'production') {
 
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+    var err = null;
+    try {
+        decodeURIComponent(req.path)
+        next();
+    }
+    catch(e) {
+        console.log("Access: " + e, req.url);
+        console.log("Req host: " + req.headers.host);
+        res.sendStatus(404);
+    }
+});
+
 app.get('*', (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 })
